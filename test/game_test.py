11ea615a6914bf531ed5d2b11352a7f087815ff9
Asiah Eskridge <aeskridge0@gmail.com>
2021-07-22 11:00:51 -0700

    Start a game test

--- test/game_test.py
+++ test/game_test.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.case import TestCase
+def new_game():
+    return None
 class game_test(unittest.TestCase):
-    def test_hello(self):
-        self.assertTrue(True)
+   
+    def test_game_is_blank(self):
+        #given
+        test_subject = new_game()
+        #when
+        #then
+         #for item in test_subject:
+        self.assertIsNone(test_subject)
+
+    def test_game_is_9_by_9(self):
+        #given
+        test_subject = new_game()
+        #when
+        #then
+         #for item in test_subject:
+        self.assertEqual(len(test_subject), 3)
+
+#return array of lens 3
 
 if __name__ == "__main__":
     unittest.main()
-
